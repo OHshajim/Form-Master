51a0a78f393fc775aa40c0fce5f743bf81a811b5
OHshajim <ajshajimmax@gmail.com>
2024-04-30 21:26:36 +0600

    finish the project

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+#Form Master 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+***diffarent way to value input  
+1. <SimpleForm>
+2. <StatefulForm>
+3. <RefForm>
+4. <HookForm>
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+#Context Use  
--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
 import './App.css'
 import Grand from './Components/Data deeply with context/Grand'
-// import SimpleForm from './Components/Form/SimpleForm'
-// import HookForm from './Components/HooksForm/HookForm'
-// import RefForm from './Components/RefForm/RefForm'
-// import StatefulForm from './Components/StatefulForm/StatefulForm'
+import SimpleForm from './Components/Form/SimpleForm'
+import HookForm from './Components/HooksForm/HookForm'
+import RefForm from './Components/RefForm/RefForm'
+import StatefulForm from './Components/StatefulForm/StatefulForm'
 import ReusableForm from './Components/ReusableForm/ReusableForm'
 
 function App() {
 
-//   const handleLogIn = data=> {
-//     console.log('Log in data',data);
-// }
-//   const handleUpdate = data => {
-//     console.log("updated data :",data);
-// }
+  const handleLogIn = data => {
+    console.log('Log in data', data);
+  }
+  const handleUpdate = data => {
+    console.log("updated data :", data);
+  }
   return (
     <>
-     
+
       <h1>Forms variant</h1>
-    {/* <SimpleForm></SimpleForm> */}
-    {/* <StatefulForm></StatefulForm> */}
-    {/* <RefForm></RefForm> */}
-    {/* <HookForm></HookForm> */}
-    {/* <ReusableForm handleSubmit={handleLogIn} submit={'log in'}>
-      <div>
-        <h3>Log In </h3>
-        <p>log in and see new massages</p>
-      </div>
-    </ReusableForm>
-    <ReusableForm handleSubmit={handleUpdate} submit={'Update'}>
-      <div>
-        <h3>Update</h3>
-        <p>update your profile and looks new</p>
-      </div>
-    </ReusableForm> */}
-    <Grand></Grand>
+      <SimpleForm></SimpleForm>
+      <StatefulForm></StatefulForm>
+      <RefForm></RefForm>
+      <HookForm></HookForm>
+      <ReusableForm handleSubmit={handleLogIn} submit={'log in'}>
+        <div>
+          <h3>Log In </h3>
+          <p>log in and see new massages</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm handleSubmit={handleUpdate} submit={'Update'}>
+        <div>
+          <h3>Update</h3>
+          <p>update your profile and looks new</p>
+        </div>
+      </ReusableForm>
+      {/* props share */}
+      <Grand></Grand>
     </>
   )
 }
